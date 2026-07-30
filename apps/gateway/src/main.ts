66c6d5f4c369f73b5d73b3095aaced8df28c9de2
yevhenbatt <310096582+yevhenbatt@users.noreply.github.com>
2026-07-30 12:33:18 +0000

    feat(gateway): add global validation

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
@@ -5,6 +6,17 @@ import { AppConfigService } from './config/app-config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
 
   const config = app.get(AppConfigService);
 
